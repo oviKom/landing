766551b8164d562f2e03698093ac29582a9a0beb
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-03-20 13:41:17 +0100

    Added random primary color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
 module.exports = {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: '#504394'
+                }
+            }
+        },
     },
     plugins: [],
 };
