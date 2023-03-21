52bf94938166194f6941590ea88b25fd51e82538
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-03-21 07:18:54 +0100

    Banner section WIP

---
+++ src/components/sections/Banner.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react'
+
+const Banner: React.FC = (): ReactElement => {
+    return (
+        <div>
+            <img src={''} />
+
+        </div>
+    )
+}
+
+export default Banner;
