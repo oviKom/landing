c4329a80c34deb218d14962781868f838f403c84
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-03-21 07:18:44 +0100

    Included Banner comp

--- src/components/views/Index.tsx
+++ src/components/views/Index.tsx
@@ -1,24 +1,10 @@
 import { ReactElement } from "react"
 import Button from "../Button"
+import Banner from "../sections/Banner"
 
 const Index: React.FC = (): ReactElement => {
     return (
-        <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea mollitia laborum at vitae dignissimos iure consectetur commodi neque in deserunt dolores incidunt, iusto aut soluta est iste omnis reprehenderit natus?
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, eveniet architecto? Nisi ea ex laudantium harum dolores fugiat adipisci, debitis sapiente dignissimos possimus consequatur quas id nesciunt obcaecati illum nobis?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil itaque velit consequuntur rerum omnis, libero, quaerat mollitia vero soluta sit iste recusandae deleniti accusantium odio eum vel ullam repellendus quisquam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis maiores eius et exercitationem nesciunt fugiat necessitatibus incidunt, ratione natus dignissimos voluptatum sed, asperiores repudiandae. Facilis explicabo quo dolor incidunt facere!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid rerum modi tenetur velit doloremque asperiores corrupti quidem nihil eligendi? Est, sit perspiciatis delectus explicabo possimus cumque. Sequi voluptate eveniet a?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quo commodi quam recusandae eos ad ab? Labore doloribus explicabo perferendis similique dolores ad, vero deleniti eveniet ullam ex, autem maxime!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus error atque nemo exercitationem dolores inventore tempora maiores cum illo repellendus. Pariatur ad sunt incidunt est doloremque commodi veritatis non maiores?
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia, quo tenetur blanditiis reiciendis suscipit harum? Aspernatur quaerat, aperiam doloremque voluptate nesciunt ad quod debitis mollitia odit ullam, laboriosam, officiis in!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quis iste vero, laudantium fugiat cum ut saepe pariatur inventore porro, in temporibus animi? Nulla quos, dolores aperiam tempore architecto eius!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio illo enim ad aliquam dolor esse fugiat non eligendi quidem voluptatum error ipsa, ut hic eum. Totam minus quam doloribus sequi!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore asperiores ipsum vitae ea, mollitia rem. Aperiam, quam animi maxime possimus architecto quibusdam, dolore quidem ullam accusamus atque culpa rem suscipit?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic perferendis eveniet ullam ipsum nostrum, est dolore iusto error animi corporis fuga delectus maiores impedit officiis, asperiores in fugiat necessitatibus tempora.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis illum minima aliquid voluptatum ex, magni quod optio ut delectus distinctio quos quas sit! Voluptas possimus pariatur, maxime earum ipsa fuga.
-            <Button text={'text'} />
-        </div>
+        <Banner />
     )
 }
 
