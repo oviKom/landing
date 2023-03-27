ed3ef3ff20e3ec235e575224524e94cc6762cf7c
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-03-27 07:07:23 +0200

    Create Card.tsx

---
+++ src/components/Card.tsx
@@ -0,0 +1,42 @@
+import classNames from 'classnames'
+import React, { ReactElement } from 'react'
+
+interface CardPropsInterface {
+    icon?: ReactElement | undefined
+    title: string,
+    description: string,
+    variant?: 'default' | 'primary'
+}
+
+const Card: React.FC<CardPropsInterface> = ({
+    icon, title, description, variant = 'default'
+}: CardPropsInterface): ReactElement => {
+    const titleColor = classNames({
+        'text-card-gray': variant === 'default',
+        'text-black': variant === 'primary',
+    });
+    const iconBackgroundClass = classNames({
+        'bg-card-gray': variant === 'default',
+        'bg-primary': variant === 'primary',
+        'text-black': variant === 'default',
+        'text-white': variant === 'primary',
+    });
+    return (
+        <div className={'inline-flex flex-col max-w-[350px] text-center place-self-center'}>
+            {icon &&
+                <div className={`${iconBackgroundClass} rounded-full w-14 h-14 flex justify-center items-center self-center mb-10`}>
+                    {icon}
+                </div>
+            }
+
+            <div className={`text-[26px] font-bold mb-6 ${titleColor}`}>
+                {title}
+            </div>
+            <div className={'text-card-purple font-semibold'}>
+                {description}
+            </div>
+        </div>
+    )
+}
+
+export default Card
