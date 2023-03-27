f3263b69e8e6ccfd49acdbc65a0f352126596855
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-03-27 07:10:05 +0200

    Create Goal.tsx

---
+++ src/components/sections/Goal.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement } from 'react';
+import goalImage_1 from '/images/goal_1.png'
+import goalImage_2 from '/images/goal_2.png'
+import goalImage_3 from '/images/goal_3.png'
+
+const Goal: React.FC = (): ReactElement => {
+    return (
+        <section id={'goal-section'} className={'mt-[calc(100vh_-_88px)] flex flex-row py-[72px] default-padding justify-between'}>
+            <div className={'max-w-[435px] flex flex-col justify-center'}>
+                <h2 className={'text-black text-[52px] font-bold mb-6'}>Célunk</h2>
+                <div className={'text-custom-gray font-semibold'}>
+                    Neque porro quisquam est, qui dolorem ipsum quia
+                    dolor sit amet, consectetur, adipisci velit, sed quia non
+                    numquam eius modi tempora incidunt ut labore et
+                    dolore magnam aliquam quaerat voluptatem. Excepteur
+                    sint occaecat cupidatat non proident, sunt in culpa qui
+                    officia deserunt mollit anim id est laborum.
+                </div>
+            </div>
+            <div className={'grid grid-cols-2 gap-[24px] w-[528px] items-end'}>
+                <img src={goalImage_1} />
+                <img src={goalImage_2} />
+                <img className={'col-span-2'} src={goalImage_3} />
+            </div>
+        </section >
+    )
+}
+
+export default Goal;
