cf432c26c28bb727ed57cdb54762d91b16ca8a47
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-03-27 07:07:36 +0200

    Added scroll function

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,33 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import Button from "../Button";
 
 const Header: React.FC = (): ReactElement => {
+    const scrollToSection = (sectionId: string) => {
+        const sectionElement: HTMLElement | null = document.getElementById(sectionId);
+
+        if (sectionElement === null) return;
+
+        setTimeout(() => {
+            window.scrollTo({ top: sectionElement.offsetTop, behavior: 'smooth' })
+        }, 1);
+    }
     return (
-        <div className={'header relative'}>
+        <div className={'header relative default-padding'}>
             <div>LOGO</div>
             <nav className={'navbar-wrapper'}>
                 <ul className={'navbar'}>
-                    <li className={'navbar-item'}><a href="#">Célunk</a></li>
-                    <li className={'navbar-item'}><a href="#">Funkciók</a></li>
-                    <li className={'navbar-item'}>Rólunk</li>
-                    <li className={'navbar-item'}>Kapcsolat</li>
+                    <li className={'navbar-item'}>
+                        <a href="#" onClick={() => scrollToSection('goal-section')}>Célunk</a>
+                    </li>
+                    <li className={'navbar-item'}>
+                        <a href="#" onClick={() => scrollToSection('functions-section')}>Funkciók</a>
+                    </li>
+                    <li className={'navbar-item'}>
+                        <a href="#" onClick={() => scrollToSection('about-us-section')}>Rólunk</a>
+                    </li>
+                    <li className={'navbar-item'}>
+                        <a href="#" onClick={() => scrollToSection('contact-section')}>Kapcsolat</a>
+                    </li>
                 </ul>
             </nav>
             <div className={'flex flex-row gap-4'}>
