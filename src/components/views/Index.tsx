fa0382a572c1ada74c534e0d503d36c3c1d4429f
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-03-27 07:09:59 +0200

    Added neccessary section components

--- src/components/views/Index.tsx
+++ src/components/views/Index.tsx
@@ -1,9 +1,19 @@
 import { ReactElement } from "react"
+import AboutUs from "../sections/AboutUs"
 import Banner from "../sections/Banner"
+import ChooseUs from "../sections/ChooseUs"
+import Functions from "../sections/Functions"
+import Goal from "../sections/Goal"
 
 const Index: React.FC = (): ReactElement => {
     return (
-        <Banner />
+        <>
+            <Banner />
+            <Goal />
+            <Functions />
+            <AboutUs />
+            <ChooseUs />
+        </>
     )
 }
 
