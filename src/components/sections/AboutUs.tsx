e29f3aa70acd6095ec6447f4085d43b57ab9d09f
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-03-27 10:53:15 +0200

    removed padding

--- src/components/sections/AboutUs.tsx
+++ src/components/sections/AboutUs.tsx
@@ -14,7 +14,7 @@ const AboutUs: React.FC = (): ReactElement => {
                 </div>
             </section>
 
-            <section className={'flex flex-col py-[72px] px-0 2xl:px-[300px]'}>
+            <section className={'flex flex-col pt-[72px] px-0 2xl:px-[300px]'}>
                 <div className={'grid grid-cols-3'}>
                     {TEAM.map((member, index) => <div className={`${index === 0 ? 'row-span-2' : ''} relative flex justify-center items-center`}>
                         <img className={'z-10 w-full h-full'} src={member.image} />
