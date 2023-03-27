5a637d26364eee94062a654a49cc6c1064642e5f
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-03-27 07:07:48 +0200

    Cleanup

--- src/components/sections/Banner.tsx
+++ src/components/sections/Banner.tsx
@@ -1,10 +1,20 @@
 import React, { ReactElement } from 'react';
+import bannerImage from '../../../public/images/banner.webp';
 
 const Banner: React.FC = (): ReactElement => {
     return (
-        <div className={'h-screen w-full absolute top-0 left-0 z-10 bg-[url("")]'}>
-            asd
-        </div >
+        <section id={'banner-section'} className={'h-screen w-full absolute top-0 left-0'}>
+            <img className={'w-full h-full object-cover absolute'} src={bannerImage} />
+
+            <div className={'relative w-full h-full flex flex-col justify-center items-center bg-black/50'}>
+                <h1 className={'text-[60px] font-bold text-center text-white leading-[68px] mb-6'}>
+                    Pedagógus és <br />szülő közös platformja.
+                </h1>
+                <div className={'text-white max-w-[400px] text-lg font-semibold'}>
+                    A csoport ajtóra helyezett elavult cetlik kiváltása a technika adta lehetőségekkel.
+                </div>
+            </div>
+        </section>
     )
 }
 
