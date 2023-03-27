03f54279af79bdb95a2f72d7f019fd07c3066a69
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-03-27 07:09:18 +0200

    Added colors, screen breakpoints for 4k

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,18 +8,31 @@ module.exports = {
         extend: {
             colors: {
                 primary: {
-                    DEFAULT: '#504394',
-                    hover: darken('#504394', 0.25)
+                    DEFAULT: '#4525F2',
+                    hover: darken('#4525F2', 0.25),
                 },
                 black: '#1C2126',
+                'custom-gray': '#737373',
+                card: {
+                    gray: '#F2F4F8',
+                    purple: '#B4A8F9'
+                }
             },
             fontFamily: {
                 'navbar': ['"Noto Sans"', 'Courier'],
                 'body': ['Montserrat', 'Courier'],
             },
             borderRadius: {
-                '100': '100px'
+                '100': '100px',
             },
+            screens: {
+                '3xl': '1750px',
+                '4xl': '1950px',
+                '5xl': '2150px',
+                '6xl': '2450px',
+                '7xl': '2850px',
+                '8xl': '3300px',
+              }
         },
     },
     plugins: [],
