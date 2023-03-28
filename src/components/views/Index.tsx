468c7faf360565389da2f1cb310b54b2b0251b69
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-03-28 18:43:49 +0200

    Included full width banner with image

--- src/components/views/Index.tsx
+++ src/components/views/Index.tsx
@@ -2,8 +2,11 @@ import { ReactElement } from "react"
 import AboutUs from "../sections/AboutUs"
 import Banner from "../sections/Banner"
 import ChooseUs from "../sections/ChooseUs"
+import FullWidthBannerSeparator from "../sections/FullWidthBannerSeparator"
 import Functions from "../sections/Functions"
 import Goal from "../sections/Goal"
+import Newsletter from "../sections/Newsletter"
+import bannerImage from '/images/mom_with_laptop.webp';
 
 const Index: React.FC = (): ReactElement => {
     return (
@@ -13,6 +16,11 @@ const Index: React.FC = (): ReactElement => {
             <Functions />
             <AboutUs />
             <ChooseUs />
+            <Newsletter />
+            <FullWidthBannerSeparator
+                image={bannerImage}
+                text={<span>Intézd a gyermek óvodai ügyeit,<br />pár kattintással bárhonnan.</span>}
+            />
         </>
     )
 }
