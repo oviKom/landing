a3b0bb0bb6825332b63a67942f94b8cd9aaf026c
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-03-28 18:43:29 +0200

    Finished sections

---
+++ src/components/sections/FullWidthBannerSeparator.tsx
@@ -0,0 +1,24 @@
+import { DocumentTextIcon } from '@heroicons/react/24/outline'
+import React, { ReactElement } from 'react'
+
+interface FWBannerSeparatorInterface {
+    image: string,
+    text: string | ReactElement,
+}
+
+const FullWidthBannerSeparator: React.FC<FWBannerSeparatorInterface> = ({
+    image, text
+}: FWBannerSeparatorInterface): ReactElement => {
+    return (
+        <div
+            style={{ backgroundImage: `url(${image})`, backgroundPosition: 'center', backgroundSize: 'cover' }}
+            className={`w-full h-fit min-h-[600px] relative flex justify-center items-center text-white`}>
+            <div className={'w-full h-full bg-black/50 absolute left-0 z-10'}></div>
+            <h2 className={'text-[46px] font-bold z-20'}>
+                {text}
+            </h2>
+        </div >
+    )
+}
+
+export default FullWidthBannerSeparator
---
+++ src/components/sections/Newsletter.tsx
@@ -0,0 +1,30 @@
+import React, { ReactElement } from 'react'
+import NewsletterSVG from '../atoms/NewsletterSVG'
+import SendIconSVG from '../atoms/SendIconSVG'
+import Button from '../Button'
+import Input from '../Input'
+
+const Newsletter: React.FC = (): ReactElement => {
+    return (
+        <section className={'flex flex-col default-padding py-[144px] bg-primary'}>
+            <div className={'bg-white rounded-2xl flex flex-row p-24'}>
+                <div className={'w-[40%]'}>
+                    <NewsletterSVG />
+                </div>
+
+                <div className={'w-[50%] flex flex-col place-self-end ml-auto'}>
+                    <h2 className={'text-[35px] font-bold pb-4'}>Hírlevél</h2>
+                    <p className={'text-custom-gray font-semibold mb-14'}>
+                        Iratkozz fel hírlevelünkre és legyél naprakész arról, hogy mi történik a Tiny Ties házatáján.
+                    </p>
+                    <div className={'flex flex-row justify-between gap-4'}>
+                        <Input placeholder={'E-mail címed'} className={'flex-shrink w-full'} />
+                        <Button text={<SendIconSVG size={24} svgClass={'absolute bottom-[10px] left-[10px]'} />} size={'circle'} />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Newsletter;
