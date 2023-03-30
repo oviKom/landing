7c4d8b1bb67e1623a9095cffe6768298acc2f12e
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-03-30 13:52:22 +0200

    Added contact conf

--- src/conf/Contact.tsx
+++ src/conf/Contact.tsx
@@ -1,3 +1,23 @@
-export const CONTACT = [
-    'gyurinadhazy@gmail.com'
-]
+import { ReactElement } from "react";
+import FacebookSVG from "../components/atoms/FacebookSVG";
+interface ContactInterface {
+    email: string;
+    socials: SocialsInterface[];
+}
+
+interface SocialsInterface {
+    icon: ReactElement;
+    url: string;
+}
+
+export const CONTACTS: ContactInterface[] = [
+    {
+        email: 'gyurinadhazy@gmail.com',
+        socials: [
+            {
+                icon: <FacebookSVG />,
+                url: 'https://www.facebook.com/gyurinadhazy'
+            }
+        ]
+    }
+];
