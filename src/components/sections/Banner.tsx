404e174cc85ea659409c3fac69b8e5753b8f7b06
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-03-30 13:59:50 +0200

    none selectable banner image

--- src/components/sections/Banner.tsx
+++ src/components/sections/Banner.tsx
@@ -4,7 +4,7 @@ import bannerImage from '/images/banner.webp';
 const Banner: React.FC = (): ReactElement => {
     return (
         <section id={'banner-section'} className={'h-screen w-full absolute top-0 left-0'}>
-            <img className={'w-full h-full object-cover absolute'} src={bannerImage} />
+            <img className={'w-full h-full object-cover absolute select-none'} src={bannerImage} />
 
             <div className={'relative w-full h-full flex flex-col justify-center items-center bg-black/50'}>
                 <h1 className={'text-[60px] font-bold text-center text-white leading-[68px] mb-6'}>
