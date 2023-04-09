901dd797c2b0aa176b4d9853944b86565d96ab83
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-04-09 08:52:56 +0200

    added buttons

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -3,6 +3,8 @@ import MenuSVG from './atoms/MenuSVG';
 import CloseSVG from './atoms/CloseSVG';
 import Navbar from './layout/Navbar';
 import { menuElements } from '../conf/MenuElements';
+import Button from './Button';
+import { scrollToSection } from '../utils/scrollToSection';
 
 const MobileMenu: React.FC = () => {
     const [menuStatus, setMenuStatus] = useState<boolean>(false);
@@ -11,9 +13,14 @@ const MobileMenu: React.FC = () => {
             <div className={`${menuStatus ? 'opacity-0' : 'opacity-100'}`} onClick={() => setMenuStatus(true)}>
                 <MenuSVG size={40} />
             </div>
-            <div className={`${menuStatus ? 'flex' : 'hidden'} relative mobile-navbar-wrapper`}>
+            <div className={`${menuStatus ? 'flex' : 'hidden'} flex-col relative mobile-navbar-wrapper`}>
                 <CloseSVG svgClass={'absolute bottom-14 mx-auto w-full'} size={40} onClick={() => setMenuStatus(false)} />
                 <Navbar menuElements={menuElements} className={'block lg:hidden mx-auto pt-12'} ulClassName={'mobile-navbar'} />
+
+                <div className={'inline-flex flex-col absolute bottom-40 gap-10 place-self-center'}>
+                    <Button text={'Írj nekünk'} onClick={() => scrollToSection(undefined, 'contact-section')} />
+                    <Button text={'Bejelentkezés'} variant={'white'} disabled={true} />
+                </div>
             </div>
         </div>
 
