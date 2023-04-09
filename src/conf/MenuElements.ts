6c5b8e1d79c9e9bbf8a08d70a20aa732acecf4e4
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-04-09 08:41:14 +0200

    Create MenuElements.ts

---
+++ src/conf/MenuElements.ts
@@ -0,0 +1,21 @@
+import { MenuElementInterface } from "../utils/interfaces";
+
+export const menuElements: MenuElementInterface[] = [
+    {
+        id: 'goal',
+        title: 'Célunk',
+    },
+    {
+        id: 'function',
+        title: 'Funkciók',
+    },
+    {
+        id: 'about-us',
+        title: 'Rólunk',
+    },
+    {
+        id: 'contact',
+        title: 'Kapcsolat',
+    },
+
+];
