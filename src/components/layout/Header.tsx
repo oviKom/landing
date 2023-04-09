f0599aa4ba5fe02c9ad6e869b88ab2523eb038ca
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-04-09 08:47:23 +0200

    Removed menu elements, added navbar comp

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,31 +1,20 @@
 import { ReactElement, useEffect } from "react";
 import Button from "../Button";
 import { scrollToSection } from "../../utils/scrollToSection";
+import MobileMenu from "../MobileMenu";
+import Navbar from "./Navbar";
+import { menuElements } from "../../conf/MenuElements";
 
 const Header: React.FC = (): ReactElement => {
     return (
         <div className={'header relative default-padding'}>
             <div>LOGO</div>
-            <nav className={'navbar-wrapper'}>
-                <ul className={'navbar'}>
-                    <li className={'navbar-item'}>
-                        <a href="#" onClick={(event) => scrollToSection(event, 'goal-section')}>Célunk</a>
-                    </li>
-                    <li className={'navbar-item'}>
-                        <a href="#" onClick={(event) => scrollToSection(event, 'functions-section')}>Funkciók</a>
-                    </li>
-                    <li className={'navbar-item'}>
-                        <a href="#" onClick={(event) => scrollToSection(event, 'about-us-section')}>Rólunk</a>
-                    </li>
-                    <li className={'navbar-item'}>
-                        <a href="#" onClick={(event) => scrollToSection(event, 'contact-section')}>Kapcsolat</a>
-                    </li>
-                </ul>
-            </nav>
-            <div className={'flex flex-row gap-4'}>
+            <Navbar menuElements={menuElements} className={'hidden lg:block'} ulClassName={'navbar'} />
+            <div className={'hidden lg:flex flex-row gap-4'}>
                 <Button text={'Írj nekünk'} onClick={() => scrollToSection(undefined, 'contact-section')} />
                 <Button text={'Bejelentkezés'} variant={'white'} disabled={true} />
             </div>
+            <MobileMenu />
         </div>
     );
 };
