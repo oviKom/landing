c997ef371ed3129699f133e92d872a8329c9bd52
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-04-09 08:41:11 +0200

    Create interfaces.ts

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,10 @@
+export interface SVGIconInterface {
+    size?: number,
+    svgClass?: string,
+    onClick?: Function | undefined
+}
+
+export interface MenuElementInterface {
+    id: string,
+    title: string,
+}
